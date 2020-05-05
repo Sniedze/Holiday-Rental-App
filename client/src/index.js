10a30a642769ac5d2c1f769b34d1757b56277f2e
petulienocka <petra.piknova8@gmail.com>
2020-05-05 13:59:16 +0200

    Routing/Components/Pages

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './app/serviceWorker';
 
-ReactDOM.render(
+  ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
